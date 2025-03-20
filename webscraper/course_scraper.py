0de9af8345d5e741a957202d2ada4921de6f1f43
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2025-03-20 17:50:31 +0100

    webscraper

---
+++ webscraper/course_scraper.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def save_content_txt():
+    # Extract the main content of the page
+    content = soup.find(id='mw-content-text').get_text()
+
+    # Save the content to a text file
+    with open('courses.txt', 'w') as file:
+        file.write(content)
+
+    print("Content saved to python_wiki.txt file")
+
+
+######################################################## START ########################################################
+
+URL = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+response = requests.get(URL)
+
+
+if response.status_code == 200:
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    
+    # Extract the title of the page
+    title = soup.title.string
+    print(f"Title of the page: {title}")
+
+
+    save_content_txt()
+
+else:
+    print("Failed to fetch the page")
