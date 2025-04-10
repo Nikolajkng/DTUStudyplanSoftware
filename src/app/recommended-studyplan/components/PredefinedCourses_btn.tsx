24c8fe4a3de031e1079bac4d09844a00d1d3880a
Frederik Udby <frederikudby556@gmail.com>
2025-04-10 15:19:27 +0200

    recommenden studyplan
    The user can now select and watch the studyplans of a few predefined ones.

---
+++ src/app/recommended-studyplan/components/PredefinedCourses_btn.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { CoursePlacement } from '../page';
+
+
+const predefinedCourses = ["Generalitet", "Algoritmitik", "Billedanalyse", "Datasikkerhed", "Kunstig intelligens", "Softwareudvikling"]
+
+
+
+interface PredefinedCoursesBtnProps {
+    onClick: (course: string) => void;
+}
+
+export default function PredefinedCourses_btn({ onClick }: PredefinedCoursesBtnProps) {
+    return (
+        <div className="m-10 flex flex-col">
+            <h2 className="text-2xl font-semibold mb-4">Anbefalede studieforløb</h2>
+            {predefinedCourses.map((course, index) => (
+                <button
+                    key={index}
+                    className="bg-blue-500 text-white px-4 py-2 rounded mb-4 hover:bg-slate-500"
+                    onClick={() => onClick(course)}
+                >
+                    {course}
+                </button>
+            ))}
+        </div>
+    )
+}
