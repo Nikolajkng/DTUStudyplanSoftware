e29dfb2f96964a970ba93dc2474aac2092848eb5
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2025-04-10 17:06:42 +0200

    uniform table

--- src/app/courses/components/CourseID.tsx
+++ src/app/courses/components/CourseID.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 export default function CourseID(props: { courseID: string }) {
     return (
-        <td className="border border-gray-300 px-4 py-2">
-            <a href={`https://kurser.dtu.dk/course/${props.courseID}`} className='text-blue-500 hover:text-blue-700 underline'>
+        <td className="border border-gray-300 py-2 text-center">
+            <a href={`https://kurser.dtu.dk/course/${props.courseID}`} className='text-blue-500 hover:text-blue-700 underline '>
                 {props.courseID}
             </a>
         </td>
