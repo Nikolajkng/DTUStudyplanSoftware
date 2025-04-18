3d197296f3175f615276143a2b15746e9897b4c3
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2025-04-18 20:16:07 +0200

    rm decimal on whole numbers

--- src/app/courses/components/CourseID.tsx
+++ src/app/courses/components/CourseID.tsx
@@ -5,9 +5,14 @@ import React from 'react'
 export default function CourseID(props: { courseID: string }) {
     return (
         <td className="border border-gray-300 py-2 text-center">
-            <a href={`https://kurser.dtu.dk/course/${props.courseID}`} className='text-blue-500 hover:text-blue-700 underline '>
-                {props.courseID}
-            </a>
+            {/* Check if courseID is 00000, then do not link */}
+            {props.courseID === '00000' ? (
+                <span className='text-gray-500'>{ }</span>
+            ) : (
+                <a href={`https://kurser.dtu.dk/course/${props.courseID}`} className='text-blue-500 hover:text-blue-700 underline '>
+                    {props.courseID}
+                </a>
+            )}
         </td>
     )
 }
--- src/app/courses/components/ECTS.tsx
+++ src/app/courses/components/ECTS.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+export default function ECTS(props: { ects: number | string }) {
+  const ectsNum = Number(props.ects)
+  const isWholeNumber = Number.isInteger(ectsNum)
 
-export default function ECTS(props: {ects: number}) {    
   return (
     <td className="border border-gray-300 px-4 py-2 text-center">
-        {props.ects}
+      <span>{isWholeNumber ? ectsNum : ectsNum.toFixed(1)}</span>
     </td>
   )
 }
