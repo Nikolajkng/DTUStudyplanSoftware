f63eb9c96e816ffd419df4dcbcfe766d06bb9790
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2025-04-21 00:19:45 +0200

    prettify: download json btn acts as other btns

--- src/app/my-studyplan/components/btn_handlers/ExportAsJsonBtn.tsx
+++ src/app/my-studyplan/components/btn_handlers/ExportAsJsonBtn.tsx
@@ -30,8 +30,8 @@ export function ExportAsJsonBtn() {
 
     return (
         <button
-            className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-            onClick={handleExportAsJson}
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+        onClick={handleExportAsJson}
         >
             Download Studieforløb som JSON
         </button>
