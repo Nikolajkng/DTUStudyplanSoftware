066d6b8595880e46fc59c1a03327f526bdfdd0d3
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2025-04-28 18:36:37 +0200

    fix: accidently rm searchquery handler

--- src/app/my-studyplan/components/courselist/SearchField.tsx
+++ src/app/my-studyplan/components/courselist/SearchField.tsx
@@ -15,6 +15,8 @@ function SearchField() {
                     type="text"
                     placeholder="Søg efter kursusID/kursusnavn"
                     className="w-80 pl-10 pr-4 py-2 rounded-full border border-gray-300 focus:ring-2 focus:ring-primary-500 focus:outline-none text-base"
+                    onChange={handleSearch}
+                    value={searchQuery}
                 />
                 <div className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400">
                     🔍
